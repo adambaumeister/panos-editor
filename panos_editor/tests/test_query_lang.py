60d642525866f17873c6f47f3f67228213363ccb
Adam Baumeister <abigserve@gmail.com>
2024-08-02 19:26:08 +1000

    More tests

--- panos_editor/tests/test_query_lang.py
+++ panos_editor/tests/test_query_lang.py
@@ -9,23 +9,10 @@ class TestStringParser:
     @pytest.mark.parametrize(
         "string, expected",
         [
-            ("x y == 1", ""),
-            ("x y == 1 AND z == 2", ""),
-        ],
-    )
-    def test_simple_parse(self, string, expected):
-        from panos_editor.query.lang import StringParser
-
-        sp = StringParser()
-        r = sp.parse(string)
-        print(r)
-
-    @pytest.mark.parametrize(
-        "string, expected",
-        [
             # Normal
-           # ("shared.address ip-netmask == 10.100.100.10", ["testhost_10.100.100.10"]),
-            ("shared.address ip-netmask == 10.100.100.10 OR name == testhost_public_ip_DYNAMIC", ["testhost_10.100.100.10", "testhost_public_ip_DYNAMIC"])
+            ("shared.address ip-netmask == 10.100.100.10", ["testhost_10.100.100.10"]),
+            ("shared.address ip-netmask == 10.100.100.10 OR name == testhost_public_ip_DYNAMIC", ["testhost_10.100.100.10", "testhost_public_ip_DYNAMIC"]),
+            ("shared.address (ip-netmask == 10.100.100.10 AND name == testhost_10.100.100.10)", ["testhost_10.100.100.10"])
         ]
     )
     def test_parse_and_query(self, string, expected, dummy_xml):
